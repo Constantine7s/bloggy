abc7a49bccb5f451916aeec1414f46aaf06129c5
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-14 11:47:59 +0900

    feat: add checkAuth error handling

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,13 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
-  const token = req.headers.authorization.replace(/Bearer\s?/, '');
-  res.send(token);
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+
   if (token) {
-    const decoded = jwt.verify(token, 'Shhhhh');
-    req.userId = decoded._id;
-    next()
+    try {
+      const decoded = jwt.verify(token, 'Shhhhh');
+
+      req.userId = decoded._id;
+      next();
+    } catch (e) {
+      return res.status(403).json({
+        message: 'Access denied', error: e.message
+      });
+    }
   } else {
-    return res.status(403).json({ message: 'Invalid token' });
+    return res.status(403).json({
+      message: 'Access denied', 
+    });
   }
 };
