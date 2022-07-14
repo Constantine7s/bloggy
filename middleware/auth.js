5391fc9bc021d138fdd77c9d848214600890ffcb
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-14 09:35:02 +0900

    feat: add create post validation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,21 @@
 import { body } from 'express-validator';
 
-const signUpValidation = [
+export const signUpValidation = [
   body('email').isEmail(),
   body('password').isLength({ min: 8 }),
   body('name').isLength({ min: 3 }),
   body('userPic').optional().isURL(),
 ];
 
-const loginValidation = [
+export const loginValidation = [
   body('email').isEmail(),
   body('password').isLength({ min: 8 }),
 
 ];
 
-
-export default signUpValidation
+export const createPostValidation = [
+  body('title').isLength({ min: 1 }).isString(),
+  body('text').isLength({ min: 3 }).isString(),
+  body('tags').isString().optional(),
+  body('imageUrl').isString().optional()
+];
