78f28d04ca246ffc08543120598af52881de5c8c
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-14 19:07:29 +0900

    feat: add get all posts functionality

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,20 +1,28 @@
 import PostModel from '../models/posts.js';
 
 export const createPost = async (req, res) => {
-    try {
-        const data = new PostModel({
-          title: req.body.title,
-          text: req.body.text,
-          imageUrl: req.body.imageUrl,
-          tags: req.body.tags,
-          author: req.userId,
-        });
-        const post = await data.save();
-        res.status(200).json(post);
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Couldn't create post" });
-      }
+  try {
+    const data = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      author: req.userId,
+    });
+    const post = await data.save();
+    res.status(200).json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldn't create post" });
+  }
 };
 
-
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Couldn't find posts" });
+  }
+};
