18a89521e2ca66fe505cba0014ad773549edde81
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-14 11:49:08 +0900

    bug: fix import

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,17 +1,20 @@
-import PostModel from '../models/posts';
+import PostModel from '../models/posts.js';
 
-export const createPost = async (req, res) => {};
-try {
-  const data = new PostModel({
-    title: req.body.title,
-    text: req.body.text,
-    imageUrl: req.body.imageUrl,
-    tags: req.body.tags,
-    author: req.userId,
-  });
-  const post = await data.save();
-  res.status(200).json(post);
-} catch (err) {
-  console.log(err);
-  res.status(500).json({ error: "Couldn't create post" });
-}
+export const createPost = async (req, res) => {
+    try {
+        const data = new PostModel({
+          title: req.body.title,
+          text: req.body.text,
+          imageUrl: req.body.imageUrl,
+          tags: req.body.tags,
+          author: req.userId,
+        });
+        const post = await data.save();
+        res.status(200).json(post);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Couldn't create post" });
+      }
+};
+
+
